9a551cdef22f9d2d7df830fae7bdbdab3b5844b1
Franco <66976969+FrancoArcajo@users.noreply.github.com>
2020-08-10 11:54:17 -0300

    Add files via upload

--- ingresoA-master/dos.js
+++ ingresoA-master/dos.js
@@ -5,5 +5,60 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
-*/function mostrar()
+*/ function mostrar() {
+    let seguir;
+    let peso;
+    let tipo;
+    let marca;
+    let precio;
+    let acumPeso = 0;
+    let liquidosCarosPrecio = 0;
+    let flagLiquidosCaros = 0;
+    let marcaLiquidosCaros;
+    let marcaSolidosLivianos;
+    let flagSolidosLivianosPeso = 0;
+    let solidosLivianosPeso = 0;
+    
+  
+    do {
+      marca = prompt("Ingrese la marca del producto");
+      precio = parseFloat(prompt("Ingrese el precio del producto"));
+      while (precio <= 0 || isNaN(precio)) {
+        precio = parseFloat(prompt("Inválido. Ingrese el precio del producto"));
+      }
+  
+      peso = parseFloat(prompt("Ingrese el peso del producto."));
+      while (peso <= 0 || isNaN(peso)) {
+        peso = parseFloat(prompt("Inválido. Ingrese el peso del producto."));
+      }
+  
+      tipo = prompt('Ingrese el tipo de producto: "solido" o "liquido"').toLowerCase();
+      while (tipo != "solido" && tipo != "liquido") {
+        tipo = prompt('Inválido. Ingrese el tipo de producto: "solido" o "liquido"').toLowerCase();
+      }
+  
+      acumPeso += peso;
+  
+      
+      switch (tipo) {
+        case "solido":
+            if (!(flagSolidosLivianosPeso) || peso < solidosLivianosPeso) {
+              solidosLivianosPeso = peso;
+              flagSolidosLivianosPeso = 1;
+              marcaSolidosLivianos = marca;
+            }
+        case "liquido":
+          if (!(flagLiquidosCaros) || precio > liquidosCarosPrecio) {
+            liquidosCarosPrecio = precio;
+            flagLiquidosCaros= 1;
+            marcaLiquidosCaros = marca;        
+      }
+    }
+      seguir = prompt("¿Desea ingresar otro producto?").toLowerCase();
+    } while (seguir === 's');
+  
+    console.log("A. El peso total de la compra es: " + acumPeso);
+    console.log("B. El producto líquido más caro es marca " + marcaLiquidosCaros + ".");
+    console.log("C. El producto sólido más liviano es marca " + marcaSolidosLivianos + ".");
+  }
 
--- ingresoA-master/tres.js
+++ ingresoA-master/tres.js
@@ -11,5 +11,83 @@ c)el promedio de personas por viaje,  que viajan en invierno
 */
 function mostrar()
 {
-	alert("tres");
+	let nombreTitular;
+	let lugar;
+	let temporada;
+	let cantidadPasajeros;
+	let contadorSalta=0;
+	let contadorCataratas=0;
+	let contadorBariloche=0;
+	let titularMasPasajeros;
+	let acumPersonas = 0;
+	let flagMasPasajeros = 0;
+	let promedioInvierno = 0;
+	let cantidadPersonas;
+	let contadorPersonasInvierno = 0;
+	let lugarMasElegido;
+
+do  {
+
+nombreTitular = prompt("Ingrese el nombre del titular");
+while (!(isNaN(nombreTitular))) {
+nombreTitular = prompt("Inválido. Ingrese el nombre del titular")
+}
+
+lugar = prompt("Ingrese el lugar").toLowerCase();
+while(lugar !="salta" && lugar!="bariloche" && lugar!="cataratas"){
+	lugar = prompt("Ingrese un destino valido").toLowerCase();
+}
+temporada = prompt("Ingrese la temporada").toLowerCase();
+while(temporada!="otoño" && temporada!="primavera" && temporada!="invierno" && temporada != "verano"){
+	temporada=prompt("Ingrese una temporada valida").toLowerCase();
+}
+cantidadPasajeros = parseInt(prompt("Ingrese la cantidad de pasajeros"));
+while(cantidadPasajeros <=0 || isNaN(cantidadPasajeros)){
+	cantidadPasajeros=parseInt(prompt("Ingrese una cantidad valida"));
+}
+
+switch(lugar){
+	case "salta":
+		contadorSalta++;
+	break;
+	case "bariloche":
+		contadorBariloche++;
+		break;
+	case "cataratas":
+		contadorCataratas++;
+		break;
+	}
+
+	if(flagMasPasajeros == 0){
+		titularMasPasajeros = cantidadPersonas;
+		titularMasPasajeros = nombreTitular;
+		flagMasPasajeros = 1
+	}
+
+	else if(cantidadPersonas > titularMasPasajeros){
+		titularMasPasajeros = cantidadPersonas;
+		titularMasPasajeros = nombreTitular;
+	}
+
+	if(temporada == "invierno"){
+		contadorPersonasInvierno ++;
+		acumPersonas = acumPersonas + cantidadPersonas;
+		
+	}
+	promedioInvierno = acumPersonas / contadorPersonasInvierno;
+ 
+	seguir = prompt("Desea ingresar otra estadia?(s/n)");
+}while(seguir == 's') 
+
+if (contadorBariloche > contadorCataratas && contadorBariloche > contadorSalta) {
+	lugarMasElegido = "Bariloche";
+} else if (contadorCataratas >= contadorBariloche && contadorCataratas > contadorSalta) {
+	lugarMasElegido = "Cataratas";
+} else {
+	lugarMasElegido = "Salta";
+}
+
+console.log(`A. El lugar mas elegido es ${lugarMasElegido}`);
+console.log(`B. El nombre del titular que lleva mas pasajeros es ${titularMasPasajeros}`);
+console.log(`C. En invierno el promedio de personas por viaje es de ${promedioInvierno}`);
 }
